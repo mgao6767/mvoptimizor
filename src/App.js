41d002c77d1225455a8665eda57f1585477aa66b
Adrian Gao <adrian.gao@outlook.com>
2018-11-07 12:14:31 +1100

    Add Steps (very basic)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import "antd/dist/antd.css";
-import DataParamsInput from "./components/DataParamsInput";
+import StepProgress from "./components/Steps";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <DataParamsInput />
+        <StepProgress />
       </div>
     );
   }
--- src/components/DataParamsInput/index.jsx
+++ src/components/DataParamsInput/index.jsx
@@ -5,31 +5,9 @@ import FrequencyPicker from "../FrequencyPicker";
 import MarketPicker from "../MarketPicker";
 import "./style.css";
 
-// const gridStyle = {
-//   width: "100%",
-//   textAlign: "center",
-//   bordered: false
-// };
-
 class DataParamsInput extends Component {
   render() {
     return (
-      //   <Card className="data-params-input">
-      //     <Card.Grid style={gridStyle}>
-      //       <Divider orientation="left">Select a market</Divider>
-      //       <MarketPicker />
-      //     </Card.Grid>
-
-      //     <Card.Grid style={gridStyle}>
-      //       <Divider orientation="left">Select data period</Divider>
-      //       <DateRangePicker />
-      //     </Card.Grid>
-
-      //     <Card.Grid style={gridStyle}>
-      //       <Divider orientation="left">Select data frequency</Divider>
-      //       <FrequencyPicker />
-      //     </Card.Grid>
-      //   </Card>
       <Card className="data-params-input">
         <Divider orientation="left">Select a market</Divider>
         <MarketPicker />

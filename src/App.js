60f0dde6c17b86924880274486c0ca1225dc9e39
Adrian Gao <adrian.gao@outlook.com>
2018-11-15 16:34:37 +1100

    build 0.0.1
    first deployment

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import "./App.css";
 import { Row, Col, Affix } from "antd";
 import "antd/dist/antd.css";
 import StepProgress from "./components/Steps";
-// import MeanVarianceChart from "./components/MeanVarianceChart";
+import ReactMarkdown from "react-markdown";
 import OptResults from "./components/Tabs";
+
+const README = `
+Developed by [Adrian Gao](https://adrian-gao.com) | Version 0.0.1
+
+**Note**: For use of University of Sydney students only due to data license (education).
+* Data: End-df-Day US stock prices since 1996, including all stocks trading on NASDAQ, AMEX, NYSE and ARCA.
+* Source: [Quandl](https://www.quandl.com/data/EOD-End-of-Day-US-Stock-Prices/documentation/product-overview), published by [Quotemedia](https://www.quotemedia.com/).
+
+**Disclaimer**: The analyses of this app are performed programmatically and do not represent investment opinions. Seek a professional financial advisor for investment advices.
+`;
 
 class App extends Component {
   // constructor(props) {
@@ -25,12 +35,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Mean-Variance Optimization</h1>
+        <h1 style={{ marginTop: 50 }}>Mean-Variance Portfolio Optimization</h1>
         <Row gutter={50}>
           <Col className="gutter-row" span={6}>
             <div className="gutter-box">
               <Affix offsetTop={0}>
                 <StepProgress />
+                <div style={{ marginTop: 50 }}>
+                  <ReactMarkdown>{README}</ReactMarkdown>
+                </div>
               </Affix>
             </div>
           </Col>

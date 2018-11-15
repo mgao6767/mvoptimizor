f0fb024bd5404aa4766ffdae4d05b18e3809c90d
Adrian Gao <adrian.gao@outlook.com>
2018-11-15 15:54:41 +1100

    Portfolio position pie chart

--- src/components/PortfolioPositionChart/index.jsx
+++ src/components/PortfolioPositionChart/index.jsx
@@ -4,11 +4,28 @@ import Plot from "react-plotly.js";
 export class PortfolioPositionChart extends Component {
   constructor(props) {
     super(props);
+    let values = [],
+      labels = [];
+    if (this.props.long) {
+      this.props.weights.forEach((w, i) => {
+        if (w >= 0) {
+          values.push(w);
+          labels.push(this.props.assets[i]);
+        }
+      });
+    } else {
+      this.props.weights.forEach((w, i) => {
+        if (w < 0) {
+          values.push(Math.abs(w));
+          labels.push(this.props.assets[i]);
+        }
+      });
+    }
     this.state = {
       data: [
         {
-          values: this.props.weights,
-          labels: this.props.assets,
+          values: values,
+          labels: labels,
           type: "pie",
           hole: 0.5,
           hoverinfo: "label+percent+name"
@@ -23,7 +40,35 @@ export class PortfolioPositionChart extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log("recevied new props", nextProps);
+    let values = [],
+      labels = [];
+    if (nextProps.long) {
+      nextProps.weights.forEach((w, i) => {
+        if (w >= 0) {
+          values.push(w);
+          labels.push(nextProps.assets[i]);
+        }
+      });
+    } else {
+      nextProps.weights.forEach((w, i) => {
+        if (w < 0) {
+          values.push(Math.abs(w));
+          labels.push(nextProps.assets[i]);
+        }
+      });
+    }
+    this.setState({
+      data: [
+        {
+          values: values,
+          labels: labels,
+          type: "pie",
+          hole: 0.5,
+          sort: false,
+          hoverinfo: "label+percent+name"
+        }
+      ]
+    });
   }
 
   render() {
@@ -33,7 +78,14 @@ export class PortfolioPositionChart extends Component {
         data={this.state.data}
         layout={this.state.layout}
         config={this.state.config}
-        useResizeHandler={true}
+        //useResizeHandler={true}
+        // onInitialized={figure => this.setState(figure)}
+        // onUpdate={figure => {
+        //   console.log("pie update, data:", figure.data);
+        //   return;
+        //   if (this.state.data === figure.data) return;
+        //   this.setState(figure);
+        // }}
       />
     );
   }

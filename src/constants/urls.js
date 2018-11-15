f6f05cb2c9cd4af34d1fa8086e4588d6f3cf1ba1
Adrian Gao <adrian.gao@outlook.com>
2018-11-15 14:12:03 +1100

    Https server

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,3 @@
-export const SERVER = 'http://adriangao.ddns.net:5000'
-export const URL_OPTIMIZATION = `${SERVER}/optimization`
-export const URL_ASSET_INFO = `${SERVER}/assets/ticker=`
+export const SERVER = "https://adrian-gao.com:5000";
+export const URL_OPTIMIZATION = `${SERVER}/optimization`;
+export const URL_ASSET_INFO = `${SERVER}/assets/ticker=`;
